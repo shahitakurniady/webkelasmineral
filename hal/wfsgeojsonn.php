f3537784babf1e8a8d040625f81f4114aabaea05
shahitakurniady <74524947+shahitakurniady@users.noreply.github.com>
2021-12-12 19:19:18 +0700

    Add files via upload

---
+++ hal/wfsgeojsonn.php
@@ -0,0 +1,9 @@
+<?php
+	// $wfsUrl = file_get_contents("http://geoportal.slemankab.go.id/geoserver/geonode/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=geonode:analisis_resiko_bencana_gunung_api&outputFormat=json");
+
+	$wfsUrl = file_get_contents("http://localhost:8080/geoserver/kelasmineral/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=kelasmineral%3APG_intan&outputFormat=application%2Fjson");
+
+	header('Content-type: application/json');
+	echo ($wfsUrl);
+	# Jika terdapat &maxFeatures=50 pada url wfs geojson, dihapus supaya jumlah feature tidak dibatasi
+?>
